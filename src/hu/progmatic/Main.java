057418228c05a6409aecdd0c787ae29928b613e1
Zsombor Leber <leberzsombor96@gmail.com>
2022-06-11 13:39:02 +0200

    Initial commit

---
+++ src/hu/progmatic/Main.java
@@ -0,0 +1,108 @@
+package hu.progmatic;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        List<MatchResults> results = loadMatches("C:\\Users\\Léber Zsombor\\IdeaProjects\\FociVB\\src\\Files\\matches_all.csv");
+
+
+        System.out.print("Number of matches: ");
+        System.out.println(loadMatches("C:\\Users\\Léber Zsombor\\IdeaProjects\\FociVB\\src\\Files\\matches_all.csv").size());
+
+
+        try {
+
+            List<MatchResults> maxgoaldiff = loadMatches("C:\\Users\\Léber Zsombor\\IdeaProjects\\FociVB\\src\\Files\\matches_all.csv");
+            System.out.println("Year of worldcup:");
+
+            Scanner scanner = new Scanner(System.in);
+            int getYear = scanner.nextInt();
+
+
+            int max = Integer.MIN_VALUE;
+
+            for (MatchResults matchResults : maxgoaldiff){
+                int sum = 0;
+                if (matchResults.getYear() == getYear) {
+                    sum = matchResults.getGoalDifference();
+
+                }
+
+                if (sum > max) {
+                    max = matchResults.getGoalDifference();
+                }
+            }
+
+
+            System.out.println("2. Maximum goal difference: " + max);
+
+
+            int counter = 0;
+
+            for (MatchResults matchResults : results){
+                if (matchResults.getGoalsByTeamA() > matchResults.getGoalsByTeamB()){
+                    if(matchResults.getYear() == getYear){
+                        counter++;
+                    }
+                }
+
+            }
+            System.out.println("3. The player has won " + counter + " bets.");
+
+
+
+            Map<String,Integer> countGoalsByStage = new TreeMap<>();
+            System.out.println("4. Total goals by stage:");
+
+            for (MatchResults matchResults : results){
+                if (matchResults.getYear() == getYear){
+                    int count = countGoalsByStage.getOrDefault(matchResults.getStage(),0);
+                    countGoalsByStage.put(matchResults.getStage(),count + (matchResults.getGoalsByTeamA() + matchResults.getGoalsByTeamB()));
+
+                }
+
+            }
+            for (String species : countGoalsByStage.keySet()){
+                System.out.println(species + ":" + countGoalsByStage.get(species));
+
+            }
+
+
+        }catch (IOException exception){
+            exception.printStackTrace();
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+    private static List<MatchResults> loadMatches(String path)throws IOException{
+        List<MatchResults> results = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))){
+            String line;
+
+            reader.readLine(); // fejléc kihagyása
+            while ((line = reader.readLine()) != null){
+                MatchResults results1 = new MatchResults(line);
+                results.add(results1);
+            }
+
+        }
+        return results;
+
+    }
+
+
+}
---
+++ src/hu/progmatic/MatchResults.java
@@ -0,0 +1,121 @@
+package hu.progmatic;
+
+public class MatchResults {
+    private WorldCups worldCups;
+    private int year;
+    private String stage;
+    private String date;
+    private String teamA;
+    private String teamB;
+    private int goalsByTeamA;
+    private int goalsByTeamB;
+    private int penaltiesA;
+    private int getPenaltiesB;
+
+    public MatchResults() {
+    }
+
+    public MatchResults(String line) {
+
+        String[] parts = line.split(";");
+
+        if (parts.length > 7){
+            this.year = Integer.parseInt(parts[0]);
+            this.stage = parts[1];
+            this.date = parts[2];
+            this.teamA = parts[3];
+            this.teamB = parts[4];
+            this.goalsByTeamA = Integer.parseInt(parts[5]);
+            this.goalsByTeamB = Integer.parseInt(parts[6]);
+            this.penaltiesA = Integer.parseInt(parts[7]);
+            this.getPenaltiesB = Integer.parseInt(parts[8]);
+        } else {
+            this.year = Integer.parseInt(parts[0]);
+            this.stage = parts[1];
+            this.date = parts[2];
+            this.teamA = parts[3];
+            this.teamB = parts[4];
+            this.goalsByTeamA = Integer.parseInt(parts[5]);
+            this.goalsByTeamB = Integer.parseInt(parts[6]);
+
+        }
+
+    }
+    public int getGoalDifference(){
+        return goalsByTeamA >= goalsByTeamB ? goalsByTeamA - goalsByTeamB : goalsByTeamB - goalsByTeamA ;
+    }
+
+
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getStage() {
+        return stage;
+    }
+
+    public void setStage(String stage) {
+        this.stage = stage;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTeamA() {
+        return teamA;
+    }
+
+    public void setTeamA(String teamA) {
+        this.teamA = teamA;
+    }
+
+    public String getTeamB() {
+        return teamB;
+    }
+
+    public void setTeamB(String teamB) {
+        this.teamB = teamB;
+    }
+
+    public int getGoalsByTeamA() {
+        return goalsByTeamA;
+    }
+
+    public void setGoalsByTeamA(int goalsByTeamA) {
+        this.goalsByTeamA = goalsByTeamA;
+    }
+
+    public int getGoalsByTeamB() {
+        return goalsByTeamB;
+    }
+
+    public void setGoalsByTeamB(int goalsByTeamB) {
+        this.goalsByTeamB = goalsByTeamB;
+    }
+
+    public int getPenaltiesA() {
+        return penaltiesA;
+    }
+
+    public void setPenaltiesA(int penaltiesA) {
+        this.penaltiesA = penaltiesA;
+    }
+
+    public int getGetPenaltiesB() {
+        return getPenaltiesB;
+    }
+
+    public void setGetPenaltiesB(int getPenaltiesB) {
+        this.getPenaltiesB = getPenaltiesB;
+    }
+}
